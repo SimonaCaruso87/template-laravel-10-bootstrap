4b3d28b15fb0e73a6d9341081845835520666313
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-09-16 00:50:20 +0200

    Edit template

--- resources/views/partials/main-footer.blade.php
+++ resources/views/partials/main-footer.blade.php
@@ -1,3 +1,32 @@
 <footer>
-    FOOTER
+    <div class="content">
+        <h1 class="title">Simona Caruso &copy; web developer
+          <div class="simona">
+            <div class="simona__item"></div>
+            <div class="simona__item"></div>
+            <div class="simona__item"></div>
+            <div class="simona__item"></div>
+          </div>
+        </h1>
+        <br>
+        <br>
+      </div>
+      <!-- partial:footer.partial.html -->
+      <div class="icon-social">
+        <ul class="wrapper">
+            <li class="icon facebook">
+                <span class="tooltip">Facebook</span>
+                <span><i class="fa-brands fa-facebook-f"></i></span>
+            </li>
+            <li class="icon instagram">
+                <span class="tooltip">Instagram</span>
+                <span><i class="fa-brands fa-instagram"></i></span>
+            </li>
+            <li class="icon github">
+                <span class="tooltip">Github</span>
+                <span><i class="fa-brands fa-github"></i></span>
+            </li>
+        </ul>
+      </div>
+  <!-- partial -->
 </footer>
