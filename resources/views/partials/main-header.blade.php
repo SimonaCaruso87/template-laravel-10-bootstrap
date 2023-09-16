9bcb6f43f56e9ea8577eae410d37c55d5a819654
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-09-17 01:35:34 +0200

    Add new img logo

--- resources/views/partials/main-header.blade.php
+++ resources/views/partials/main-header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-dark border-bottom border-black" data-db-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#"><img src="{{ Vite::asset('resources/img/logo.png') }}" class="img-fluid" alt=""></a>
+            <a class="navbar-brand text-white" href="#"><img src="{{ asset('resources/img/logoNew.png') }}" class="img-fluid" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
