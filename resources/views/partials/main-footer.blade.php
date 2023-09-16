5a3351df9db39edafaf84a5a788e7cc22930877b
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-09-16 03:04:33 +0200

    Modifica footer icon social

--- resources/views/partials/main-footer.blade.php
+++ resources/views/partials/main-footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="content">
-        <h1 class="title">Simona Caruso &copy; web developer
+        <h1 class="title border border-black rounded-5 p-2 mt-2">&copy; By - Simona Caruso - Web Dev
           <div class="simona">
             <div class="simona__item"></div>
             <div class="simona__item"></div>
@@ -8,25 +8,23 @@
             <div class="simona__item"></div>
           </div>
         </h1>
-        <br>
-        <br>
+        <!-- partial:footer.partial.html -->
+        <div class="icon-social">
+            <ul class="wrapper">
+                <li class="icon facebook">
+                    <span class="tooltip">Facebook</span>
+                    <span><i class="fa-brands fa-facebook-f"></i></span>
+                </li>
+                <li class="icon instagram">
+                    <span class="tooltip">Instagram</span>
+                    <span><i class="fa-brands fa-instagram"></i></span>
+                </li>
+                <li class="icon github">
+                    <span class="tooltip">Github</span>
+                    <span><i class="fa-brands fa-github"></i></span>
+                </li>
+            </ul>
+        </div>
+        <!-- partial -->
       </div>
-      <!-- partial:footer.partial.html -->
-      <div class="icon-social">
-        <ul class="wrapper">
-            <li class="icon facebook">
-                <span class="tooltip">Facebook</span>
-                <span><i class="fa-brands fa-facebook-f"></i></span>
-            </li>
-            <li class="icon instagram">
-                <span class="tooltip">Instagram</span>
-                <span><i class="fa-brands fa-instagram"></i></span>
-            </li>
-            <li class="icon github">
-                <span class="tooltip">Github</span>
-                <span><i class="fa-brands fa-github"></i></span>
-            </li>
-        </ul>
-      </div>
-  <!-- partial -->
 </footer>
